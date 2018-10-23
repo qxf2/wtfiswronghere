e8e17da9e4af6615ef78635997085342e2d14a9a
nilaya1234 <nilaya@qxf2.com>
2018-10-23 16:24:42 +0530

    added docstring

--- 10_challenge/fizzbuzz.py
+++ 10_challenge/fizzbuzz.py
@@ -1,3 +1,14 @@
+"""
+We will use this script to teach Python to absolute beginners
+The script is an example of Fizz-Buzz implemented in Python
+
+The FizzBuzz problem: 
+For all integers between 1 and 99 (include both):
+    # print fizz for multiples of 3
+    # print buzz for multiples of 5 
+    # print fizzbuzz for multiples of 3 and 5"
+"""
+
 def fizzbuzz(max_num):
     "This method implements FizzBuzz"
     # Google for 'range in python' to see what it does
