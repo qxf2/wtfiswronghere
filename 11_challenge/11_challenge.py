6d193b8c98502fddf338ab357756cb46be654934
raddhakrishnan <36443969+raddhakrishnan@users.noreply.github.com>
2018-10-23 16:47:02 +0530

    Added challenge 11, fizzbuzz is not printed (#7)
    * Added challenge 11, fizzbuzz is not printed
    
    * Updated for error message
    
    * Updated error message
    
    Looks good @raddhakrishnan - thank you!

---
+++ 11_challenge/11_challenge.py
@@ -0,0 +1,26 @@
+"""
+We will use this script to teach Python to absolute beginners
+The script is an example of Fizz-Buzz implemented in Python
+
+The FizzBuzz problem: 
+For all integers between 1 and 99 (include both):
+    # print fizz for multiples of 3
+    # print buzz for multiples of 5 
+    # print fizzbuzz for multiples of 3 and 5"
+"""
+
+def fizzbuzz(max_num):
+    "This method implements FizzBuzz"
+    # Google for 'range in python' to see what it does
+    for i in range(1,max_num):
+        # % or modulo division gives you the remainder 
+        if i%3==0:
+            print(i,"fizz")
+        elif i%5==0:
+            print(i,"Buzz")     
+        elif i%3==0 and i%5==0:
+            print(i,"fizzbuzz")
+
+#----START OF SCRIPT
+if __name__=='__main__':
+    fizzbuzz(100)
