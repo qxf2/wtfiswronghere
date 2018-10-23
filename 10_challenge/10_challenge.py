a578d65c7390fe0d29f378a2bafeabf64565a189
nilaya123 <nilaya@qxf2.com>
2018-10-23 16:48:11 +0530

    commiting changes (#8)
    * commiting changes
    
    * added docstring
    
    Looks good @nilaya123 - thank you!

---
+++ 10_challenge/10_challenge.py
@@ -0,0 +1,15 @@
+"""
+We will use this script to teach Python to absolute beginners
+The script is an example of Fizz-Buzz implemented in Python
+
+The FizzBuzz problem: 
+For all integers between 1 and 99 (include both):
+    # print fizz for multiples of 3
+    # print buzz for multiples of 5 
+    # print fizzbuzz for multiples of 3 and 5"
+"""
+import fizzbuzz
+
+#----START OF SCRIPT
+if __name__=='__main__':
+    fizzbuzz(100)
