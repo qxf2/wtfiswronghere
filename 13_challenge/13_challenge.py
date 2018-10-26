0aae3f985c832955b08030d8ea2a53c6b4a0027f
Arunkumar <mak@qxf2.com>
2018-10-26 10:29:35 +0530

    changes are made for 13_challenge

--- 13_challenge/13_challenge.py
+++ 13_challenge/13_challenge.py
@@ -1,9 +1,14 @@
-"Add typo in a method name"
+"""
+Add typo in a method name
+Forgot colon in the for
+Miss coding in double quote
+"""
 
 
 def fizzbuzz(self,max_num):
     "This method implements FizzBuzz"
-    print('fizzbuzz')
+    fizze=fizzbuzz
+    fizze(100)
     # adding some redundant declarations on purpose
     # we will make our script 'tighter' in one of coming exercises
     three_mul = 'fizz'
@@ -12,11 +17,11 @@ def fizzbuzz(self,max_num):
     num2 = 5 
 
     # Google for 'range in python' to see what it does
-    for i in range(1,max_num):
+    for i in range(1,max_num)
         # % or modulo division gives you the remainder 
         if i%num1==0 and i%num2==0:
             print(i,three_mul+five_mul)
-        elif i%num1=0:
+        elif i%num1==0:
             print(i,three_mul)
         elif i%num2==0:
             print(i,five_mul)
