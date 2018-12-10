7c6c9a30659bf5f8fba64de64bcc04cdbaa87e25
Bradley Pelton <bradleypelton@gmail.com>
2018-12-10 09:12:10 -0500

    Missing Indentation
    Title

--- 04_challenge/04_challenge.py
+++ 04_challenge/04_challenge.py
@@ -23,7 +23,7 @@ def fizzbuzz(max_num):
     for i in range(1,max_num):
         # % or modulo division gives you the remainder 
         if i%num1==0 and i%num2==0:
-        print(i,three_mul+five_mul)
+            print(i,three_mul+five_mul)
         elif i%num1==0:
             print(i,three_mul)
         elif i%num2==0:
