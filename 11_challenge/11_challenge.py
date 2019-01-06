5368b2bbeb0594db271d8935fe2cb3f44630bfe9
Bradley Pelton <bradleypelton@gmail.com>
2019-01-05 21:54:28 -0500

    Update 11_challenge.py

--- 11_challenge/11_challenge.py
+++ 11_challenge/11_challenge.py
@@ -14,12 +14,12 @@ def fizzbuzz(max_num):
     # Google for 'range in python' to see what it does
     for i in range(1,max_num):
         # % or modulo division gives you the remainder 
-        if i%3==0:
-            print(i,"fizz")
-        elif i%5==0:
-            print(i,"Buzz")     
-        elif i%3==0 and i%5==0:
+        if i%3==0 and i%5==0:
             print(i,"fizzbuzz")
+        elif i%5==0:
+            print(i,"buzz")     
+        elif i%3 == 0:
+            print(i,"fizz")
 
 #----START OF SCRIPT
 if __name__=='__main__':
