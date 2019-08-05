c2ab60c92ae0ec0fcbaed50cf865a8a1a3d05eee
PreedhiVivek <preedhivivek@gmail.com>
2019-08-05 13:25:11 +0530

    Fixed the file not found issue

--- 05_challenge/05_challenge.py
+++ 05_challenge/05_challenge.py
@@ -9,7 +9,7 @@ For all integers between 1 and 99 (include both):
     # print buzz for multiples of 5 
     # print fizzbuzz for multiples of 3 and 5"
 """
-
+import os 
 def fizzbuzz(max_num):
     "This method implements FizzBuzz"
     
@@ -17,8 +17,11 @@ def fizzbuzz(max_num):
     # we will make our script 'tighter' in one of coming exercises
     three_mul = 'fizz'
     five_mul = 'buzz'
-    with open('mifile.txt','r') as f:
-        print 'i have created'
+    print("FILE PATH:",__file__)
+    filename = os.path.abspath(os.path.join(os.path.dirname(__file__),'myfile.txt'))
+    
+    with open(filename,'r') as f:
+        print ('i have created')
         num1 = int(f.readline())   
         num2=int(f.readline())        
         max_num = int(f.readline())
@@ -32,7 +35,7 @@ def fizzbuzz(max_num):
             print(i,three_mul)
         elif i%num2==0:
             print(i,five_mul)
-
+ 
 #----START OF SCRIPT
 if __name__=='__main__':
     fizzbuzz(100)
