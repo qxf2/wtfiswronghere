1697a126f62a638c3e4d58d8599a62a43765be83
Rahul.Bhave <rahul.bhave@qxf2.com>
2019-09-18 11:32:09 +0530

    Changes made to learn all python tutorials self study

--- 10_challenge/10_challenge.py
+++ 10_challenge/10_challenge.py
@@ -8,7 +8,7 @@ For all integers between 1 and 99 (include both):
     # print buzz for multiples of 5 
     # print fizzbuzz for multiples of 3 and 5"
 """
-import fizzbuzz
+from fizzbuzz import fizzbuzz
 
 #----START OF SCRIPT
 if __name__=='__main__':
--- 11_challenge/11_challenge.py
+++ 11_challenge/11_challenge.py
@@ -14,12 +14,14 @@ def fizzbuzz(max_num):
     # Google for 'range in python' to see what it does
     for i in range(1,max_num):
         # % or modulo division gives you the remainder 
-        if i%3==0:
+        
+        if i%3==0 and i%5==0:
+            print(i,"fizzbuzz")       
+        elif i%3==0:
             print(i,"fizz")
         elif i%5==0:
             print(i,"Buzz")     
-        elif i%3==0 and i%5==0:
-            print(i,"fizzbuzz")
+        
 
 #----START OF SCRIPT
 if __name__=='__main__':
