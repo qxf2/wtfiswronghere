7f8f4c58ee6aba6421c24027cd54ed21d365ab2d
PreedhiVivek <preedhivivek@gmail.com>
2019-09-20 11:59:25 +0530

    fixed the module name issue

--- 10_challenge/10_challenge.py
+++ 10_challenge/10_challenge.py
@@ -8,8 +8,8 @@ For all integers between 1 and 99 (include both):
     # print buzz for multiples of 5 
     # print fizzbuzz for multiples of 3 and 5"
 """
-import fizzbuzz
+import fizzbuzz as f1
 
 #----START OF SCRIPT
 if __name__=='__main__':
-    fizzbuzz(100)
+    f1.fizzbuzz(100)
