e0bbb3f9e0ac9214113970bca4fe613517d04a78
Annapoorani Bala <Arunkumar>
2020-02-04 10:32:34 +0530

    Added some lines in the challenge 01

--- 01_challenge/01_challenge.py
+++ 01_challenge/01_challenge.py
@@ -14,14 +14,15 @@ def fizzbuzz(max_num):
     
     # adding some redundant declarations on purpose
     # we will make our script 'tighter' in one of coming exercises
-    three_mul = 'fizz'
-    five_mul = 'buzz'
+    three_mul = 'qxf2'
+    five_mul = 'services'
     num1 = 3
     num2 = 5 
 
     # Google for 'range in python' to see what it does
-    for i in range(1,max_num):
-        # % or modulo division gives you the remainder 
+    for i in range(1,max_num+1):
+        # % or modulo division gives you the remainder
+        print(max_num,i) 
         if i%num1==0 and i%num2==0:
             print(i,three_mul+five_mul)
         elif i%num1==0:
