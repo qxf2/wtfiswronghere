86c42956b6df16f5b8ce7fd7321d00a1836c590b
ambigai-rajan <ambigairajan38@gmail.com>
2022-12-02 17:06:05 +0530

    errors cleared

--- 04_challenge/04_challenge.py
+++ 04_challenge/04_challenge.py
@@ -22,10 +22,10 @@ def fizzbuzz(max_num):
     # Google for 'range in python' to see what it does
     for i in range(1,max_num):
         # % or modulo division gives you the remainder 
-        if i%num1==0 and i%num2==0:
-        print(i,three_mul+five_mul)
+        if i%num1==0 and i%num2==0:{
+        print(i,three_mul+five_mul)}
         elif i%num1==0:
-            print(i,three_mul)
+          print(i,three_mul)
         elif i%num2==0:
             print(i,five_mul)
 
